343ef4fe3cdc432b7aa5c3e910e4878d7064edf1
asato-mi <86206312+asato-mi@users.noreply.github.com>
2021-07-14 11:02:49 -0500

    Code updates for functionality

--- tipcalc.js
+++ tipcalc.js
@@ -1,18 +1,42 @@
-document.querySelector('#tip-form').onchange = function(){
+window.onload = () =>
+	//the function called when Calculate button is clicked.
+	{
+		/*calling a function calculateTip
+		which will calculate the tip for the bill.*/
+		document.querySelector('#calculate').onclick = calculateTip;
+	}
 
-    var bill = Number(document.getElementById('billTotal').value);
-    var tip = document.getElementById('tipInput').value;
-    document.getElementById('tipOutput').innerHTML = `${tip}%`;
-    var tipValue = bill * (tip/100)
-    var finalBill = bill + tipValue
-  console.log(finalBill)
-  var tipAmount = document.querySelector('#tipAmount')
-  var totalBillWithTip = document.querySelector('#totalBillWithTip')
-  
-  tipAmount.value = tipValue.toFixed(2);
-   totalBillWithTip.value =finalBill.toFixed(2);
-  
-   //Show Results
-  
-    document.getElementById('results').style.display='block'
-  }
+function calculateTip() {
+	/*assing values of ID : amount, person and service to
+	variables for further calculations.*/
+	let amount = document.querySelector('#amount').value;
+	let persons = document.querySelector('#persons').value;
+	let service = document.querySelector('#services').value;
+
+	console.log(service);
+	/*if statement will work when user presses
+		calculate without entering values. */
+	//so will display an alert box and return.
+	if (amount === '' && service === 'Select') {
+		alert("Please enter valid values");
+		return;
+	}
+
+	//now we are checking number of persons
+	if (persons === '1')
+	//if there is only one person then we need not to display each.
+		document.querySelector('#each').style.display = 'none';
+	else
+	//if there are more than one person we will display each.
+		document.querySelector('#each').style.display = 'block';
+
+	/*calculating the tip by multiplying total-bill and type of
+	service; then dividing it by number of persons.*/
+	//fixing the total amount upto 2 digits of decimal
+	let total = (amount * service) / persons;
+	total = total.toFixed(2);
+
+	//finally displaying the tip value
+	document.querySelector('.tip').style.display = 'block';
+	document.querySelector('#total').innerHTML = total;
+}
